4ea9acad8f65379b36d09d6e88fbe3156a87d070
dankelemen <dkelemen@gmail.com>
2017-01-26 15:24:37 -0500

    Initial framework
    Setting up github with arduino skeleton

---
+++ finish_line/finish_line.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // Pinewood Derby Finish Line
+  // Daniel Kelemen
+  // Jan, 2017
+  // Pack 23 Suffern
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
